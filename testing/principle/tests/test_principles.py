207518ff1c4931200a2c5bad3882051c00061c15
waqque <tanos_hanos@mail.ru>
2026-03-05 12:30:57 +0800

    Initial commit for testing prectice. Added test for addition

---
+++ testing/principle/tests/test_principles.py
@@ -0,0 +1,13 @@
+import sys 
+sys.path.append("../src")
+
+#TODO make it with 'pip insfall -e.'
+
+from math_demo import add
+def test_addition():
+    assert 2 + 2 == 4
+    print("test ADDIOTION PASSED")
+
+
+if name == "__main__":
+    test_addition()
