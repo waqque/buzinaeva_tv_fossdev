92d755439d407a5509870af3f84b202059ce16ee
waqque <tanos_hanos@mail.ru>
2026-04-02 12:26:25 +0800

    Added DOMAIN file

---
+++ docs_domain/src/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello from practice!")
+
+if __name__ == "__main__":
+    main()
---
+++ docs_domain/tests/test_main.py
@@ -0,0 +1,5 @@
+"""Sample test file."""
+from src.main import main
+
+def test_main():
+    assert main() is None
